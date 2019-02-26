72b09ee898c364aa20930c763d39438023534ef3
luckyrat <luckyrat@musites.com>
2019-02-26 23:11:48 +0000

    update deps

--- tests/handler.test.ts
+++ tests/handler.test.ts
@@ -7,7 +7,7 @@ beforeAll(() => {
 describe("utils", async () => {
 
     test("hex roundtrips through base64", async () => {
-        const b64 = "9E0mTE3/KZvKCvuMSDOvsFe+BNSH9oz812y3BsZn4/E=";
+        // b64 = "9E0mTE3/KZvKCvuMSDOvsFe+BNSH9oz812y3BsZn4/E=";
         const hex = "f44d264c4dff299bca0afb8c4833afb057be04d487f68cfcd76cb706c667e3f1";
 
         const temp = hex2base64(hex);
